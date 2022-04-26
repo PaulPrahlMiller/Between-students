b0ce9d1b1be2d3310e04c07cf3bcecdbdd5a39fd
chinita226 <@chinita226>
2022-04-26 22:07:24 +0200

    fixing upload images inside database

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,46 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import Welcome from './components/welcome/Welcome';
+import AddProduct from './components/welcome/AddProduct';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [message, setMessage] = useState('');
+  // const [message, setMessage] = useState('');
+  // const [images, setImages] = useState([]);
+  // const [imageURLs, setImageURLs] = useState([]);
 
-  useEffect(() => {
-    const fetchFromApi = () => {
-      fetch('/api')
-        .then((res) => res.json())
-        .then((data) => setMessage(data.message));
-    };
-    fetchFromApi();
-  }, [message]);
+  // function onImageChange(e) {
+  //   setImages([...e.target.files]);
+  //   console.log('====================================');
+  //   console.log(e.target.files);
+  //   console.log('====================================');
+  // }
+
+  // useEffect(() => {
+  //   const fetchFromApi = () => {
+  //     fetch('/api')
+  //       .then((res) => res.json())
+  //       .then((data) => setMessage(data.message));
+  //   };
+  //   fetchFromApi();
+  // }, [message]);
+
+  // useEffect(() => {
+  //   if (images.length < 1) return;
+  //   const newImageUrls = [];
+  //   images.forEach((image) => newImageUrls.push(URL.createObjectURL(image)));
+  //   console.log('====================================');
+  //   console.log(newImageUrls);
+  //   console.log('====================================');
+  //   setImageURLs(newImageUrls);
+  // }, [images]);
 
   return (
     <div className='App'>
-      <h1 style={{ textAlign: 'center' }}>Fullstack MERN application</h1>
-      <Welcome message={message} />
+      Hello to MERN APP <br />
+      {/* <h1 style={{ textAlign: 'center' }}>Fullstack MERN application</h1>
+      <Welcome message={message} /> */}
+      <AddProduct></AddProduct>
     </div>
   );
 }
---
+++ models/Product.js
@@ -0,0 +1,22 @@
+const { default: mongoose } = require('mongoose');
+const { Schema, model } = require('mongoose');
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    min: 2,
+    max: 32,
+    required: true
+  },
+  description: {
+    type: String,
+    min: 6,
+    max: 255,
+    required: true
+  },
+  productImage: { type: String, required: true },
+  postDate: { type: Date, default: Date.now }
+});
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
