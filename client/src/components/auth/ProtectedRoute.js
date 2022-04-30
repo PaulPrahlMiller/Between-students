0e4e5b691f0af8d8b6d026ea388f65af55ea6923
Paul Miller <paul_john.miller0008@stud.hkr.se>
2022-04-30 10:35:21 +0200

    temporarily removed auth requirement for protected route

--- client/src/components/auth/ProtectedRoute.js
+++ client/src/components/auth/ProtectedRoute.js
@@ -4,7 +4,7 @@ import { useAuth } from '../../hooks/useAuth';
 
 const ProtectedRoute = ({ children }) => {
   const [authState] = useAuth();
-  const isAuthenticated = true;
+  const isAuthenticated = true; // This should be changed back to a destructured value from authState
   if (!isAuthenticated) {
     return <Navigate to='/login' replace={true} />;
   }
