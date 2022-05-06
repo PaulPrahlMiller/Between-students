9e674a5e1126bc34d1e545d6e79fde09b991f08d
Paul Miller <paul_john.miller0008@stud.hkr.se>
2022-05-06 10:09:18 +0200

    Added new api routing to server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const apiRoutes = require('./routes/api');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const productRoutes = require('./routes/products');
 const connectDatabase = require('./config/db');
 const PORT = process.env.PORT || 5000;
 const signale = require('signale');
@@ -21,7 +23,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.use('/api', apiRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => {
   signale.success(`Server listening on port ${PORT}`);
