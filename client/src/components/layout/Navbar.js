ad0c56b71e1127b3aaee47a14480e4690a3a2df7
Paul Miller <paul_john.miller0008@stud.hkr.se>
2022-05-09 22:36:10 +0200

    Replaced anchor tags with Links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,42 +1,46 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 //import { log } from 'signale';
 
-const accountIcon = require('../../assets/account_icon.jpg')
+const accountIcon = require('../../assets/account_icon.jpg');
 
 const Navbar = () => {
-    const pathname = window.location.href.split("/").pop();
-    const [ProductState, productDispatch] = useProducts();
-    const navigate = useNavigate();
-    const handleClick = (e) => {
-        console.log(e.target.innerText)
-        navigate(`/categories/${e.target.innerText}`);
-    }
-    return <div className="topnav">
-        <a className={pathname === '' && "active"} href="/">Home</a>
+  const pathname = window.location.href.split('/').pop();
+  const [ProductState, productDispatch] = useProducts();
+  const navigate = useNavigate();
+  const handleClick = (e) => {
+    console.log(e.target.innerText);
+    navigate(`/categories/${e.target.innerText}`);
+  };
+  return (
+    <div className='topnav'>
+      <Link to='/' className={pathname === '' && 'active'}>
+        Home
+      </Link>
 
-        <div className="subnav" onClick={handleClick}>
-            <button className="subnavbtn">Categories</button>
-            <div className="subnav-content">
-                {ProductState.categories.map((cat) => {
-                    return (
-                        <option  value={cat} key={cat.id}>
-                            {cat}
-                        </option>
-                    );
-                })}
-
-            </div>
+      <div className='subnav' onClick={handleClick}>
+        <button className='subnavbtn'>Categories</button>
+        <div className='subnav-content'>
+          {ProductState.categories.map((cat) => {
+            return (
+              <option value={cat} key={cat.id}>
+                {cat}
+              </option>
+            );
+          })}
         </div>
-        <a className={pathname === "about" && "active"} href="/about">About Us</a>
-        <div className="accountnav">
-            <a className={pathname === "account" && "active"} href="/account">
-                <img src={accountIcon} className="accounticon" alt="account_icon" />
-            </a>
-        </div>
-    </div>;
+      </div>
+      <Link to='/about' className={pathname === 'about' && 'active'}>
+        About Us
+      </Link>
+      <div className='accountnav'>
+        <Link to='/account' className={pathname === 'account' && 'active'}>
+          <img src={accountIcon} className='accounticon' alt='account_icon' />
+        </Link>
+      </div>
+    </div>
+  );
 };
-
 
 export default Navbar;
