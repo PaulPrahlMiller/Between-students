d001364b2ab2aafaa183ee78d481c3660c710af1
Zijie Feng <jimeswals@gmail.com>
2022-05-11 18:12:54 +0200

    update about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,7 +4,7 @@ const TeamPhoto_Tian = require('../../src/assets/Tian.jpg')
 const TeamPhoto_Zijie = require('../../src/assets/Zijie.jpg')
 const TeamPhoto_Paul = require('../../src/assets/Tian.jpg')
 const TeamPhoto_Frank = require('../../src/assets/Tian.jpg')
-const TeamPhoto_Meron = require('../../src/assets/Tian.jpg')
+const TeamPhoto_Meron = require('../../src/assets/Meron.jpg')
 
 
 const About = () => {
@@ -37,36 +37,36 @@ const About = () => {
 {/*3 profolio*/}
         <div className="aboutcontainer">
             <div className="aboutphoto1">
-                {<img src={TeamPhoto_Tian} className="aboutphoto1" alt="about_photo" />}
+                {<img src={TeamPhoto_Meron} className="aboutphoto1" alt="about_photo" />}
             </div>
             <div className="abouttext1">
-                <h1> Tian Yao</h1>
+                <h1 className="aboutname"> Meron Habtemichael</h1>
                 <h1 className="abouttitle1"> Full-stack developer</h1>
-                <p className="aboutintrotext1"> She is studying in Computer Science</p>
+                <p className="aboutintrotext1"> He is a CS student.</p>
             </div>
         </div>
 
 {/*4 profolio*/}
         <div className="aboutcontainer">
             <div className="aboutphoto1">
-                {<img src={TeamPhoto_Tian} className="aboutphoto1" alt="about_photo" />}
+                {<img src={TeamPhoto_Paul} className="aboutphoto1" alt="about_photo" />}
             </div>
             <div className="abouttext1">
-                <h1> Tian Yao</h1>
-                <h1 className="abouttitle1"> Full-stack developer</h1>
-                <p className="aboutintrotext1"> She is studying in Computer Science</p>
+                <h1 className="aboutname"> Full name</h1>
+                <h1 className="abouttitle1"> title</h1>
+                <p className="aboutintrotext1"> info</p>
             </div>
         </div>
 
 {/*5 profolio*/}
         <div className="aboutcontainer">
             <div className="aboutphoto1">
-                {<img src={TeamPhoto_Tian} className="aboutphoto1" alt="about_photo" />}
+                {<img src={TeamPhoto_Frank} className="aboutphoto1" alt="about_photo" />}
             </div>
             <div className="abouttext1">
-                <h1> Tian Yao</h1>
-                <h1 className="abouttitle1"> Full-stack developer</h1>
-                <p className="aboutintrotext1"> She is studying in Computer Science</p>
+                <h1 className="aboutname"> Full name</h1>
+                <h1 className="abouttitle1"> title</h1>
+                <p className="aboutintrotext1"> info</p>
             </div>
         </div>
 
