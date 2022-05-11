325810380a22e79aa4aa9910d31238c2d31a71d9
chinita226 <@chinita226>
2022-05-11 02:26:30 +0200

    changed styles with nav and header

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,42 +1,45 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
 import { useNavigate } from 'react-router-dom';
-//import { log } from 'signale';
 
-const accountIcon = require('../../assets/account_icon.jpg')
+const accountIcon = require('../../assets/account_icon.jpg');
 
 const Navbar = () => {
-    const pathname = window.location.href.split("/").pop();
-    const [ProductState, productDispatch] = useProducts();
-    const navigate = useNavigate();
-    const handleClick = (e) => {
-        console.log(e.target.innerText)
-        navigate(`/categories/${e.target.innerText}`);
-    }
-    return <div className="topnav">
-        <a className={pathname === '' && "active"} href="/">Home</a>
+  const pathname = window.location.href.split('/').pop();
+  const [ProductState, productDispatch] = useProducts();
+  const navigate = useNavigate();
+  const handleClick = (e) => {
+    console.log(e.target.innerText);
+    navigate(`/categories/${e.target.innerText}`);
+  };
+  return (
+    <div class='topnav'>
+      <a class={pathname === '' && 'active'} href='/'>
+        Home
+      </a>
 
-        <div className="subnav" onClick={handleClick}>
-            <button className="subnavbtn">Categories</button>
-            <div className="subnav-content">
-                {ProductState.categories.map((cat) => {
-                    return (
-                        <option  value={cat} key={cat.id}>
-                            {cat}
-                        </option>
-                    );
-                })}
-
-            </div>
+      <div class='subnav' onClick={handleClick}>
+        <button class='subnavbtn'>Categories</button>
+        <div class='subnav-content'>
+          {ProductState.categories.map((cat) => {
+            return (
+              <option value={cat} key={cat.id}>
+                {cat}
+              </option>
+            );
+          })}
         </div>
-        <a className={pathname === "about" && "active"} href="/about">About Us</a>
-        <div className="accountnav">
-            <a className={pathname === "account" && "active"} href="/account">
-                <img src={accountIcon} className="accounticon" alt="account_icon" />
-            </a>
-        </div>
-    </div>;
+      </div>
+      <a class={pathname === 'about' && 'active'} href='/about'>
+        About Us
+      </a>
+      <div class='accountnav'>
+        <a class={pathname === 'account' && 'active'} href='/account'>
+          <img src={accountIcon} class='accounticon' alt='account_icon' />
+        </a>
+      </div>
+    </div>
+  );
 };
-
 
 export default Navbar;
