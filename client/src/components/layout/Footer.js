e3817db0a2c34fd7145e8a8348171c3c64b5aa87
chinita226 <@chinita226>
2022-05-12 22:56:27 +0200

    fix footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,25 +1,68 @@
 import React from 'react';
-
-const footerPhoto = require('../../assets/footer_photo.jpg');
-
-const facebookIcon = require('../../assets/facebook.jpg');
-const instagramIcon = require('../../assets/instagram.jpg');
-const twitterIcon = require('../../assets/twitter.jpg');
+import { Link } from 'react-router-dom';
+import { MDBFooter, MDBIcon } from 'mdb-react-ui-kit';
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <img src={footerPhoto} className='footerphoto' alt='footer_photo' />
-      <div className='footerphototext'>
-        <span>Follow us on social media!</span>
-        <br />
-        <img src={instagramIcon} className='instagramicon' alt='instagram_icon' />
-        <img src={facebookIcon} className='facebookicon' alt='facebook_icon' />
-        <img src={twitterIcon} className='twittericon' alt='twitter_icon' />
-        <br />
-        <span>2022 Student Life</span>
+    <MDBFooter className='bg-light text-center text-white'>
+      <div className='container p-4 pb-0'>
+        <section className='mb-4'>
+          <Link
+            to={'/facebook'}
+            className='btn btn-primary btn-floating m-1'
+            style={{ backgroundColor: '#3b5998' }}
+          >
+            <MDBIcon fab icon='facebook-f' />
+          </Link>
+          <Link
+            to={'/twitter'}
+            className='btn btn-primary btn-floating m-1'
+            style={{ backgroundColor: '#55acee' }}
+          >
+            <MDBIcon fab icon='twitter' />
+          </Link>
+
+          <Link
+            to={'/google'}
+            className='btn btn-primary btn-floating m-1'
+            style={{ backgroundColor: '#dd4b39' }}
+          >
+            <MDBIcon fab icon='google' />
+          </Link>
+          <Link
+            to={'/instagram'}
+            className='btn btn-primary btn-floating m-1'
+            style={{ backgroundColor: '#ac2bac' }}
+          >
+            <MDBIcon fab icon='instagram' />
+          </Link>
+
+          <Link
+            to={'/linkedin'}
+            className='btn btn-primary btn-floating m-1'
+            style={{ backgroundColor: '#0082ca' }}
+          >
+            <MDBIcon fab icon='linkedin-in' />
+          </Link>
+
+          <Link
+            to={'/github'}
+            className='btn btn-primary btn-floating m-1'
+            style={{ backgroundColor: '#333333' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='github' />
+          </Link>
+        </section>
       </div>
-    </div>
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2022 Copyright:
+        <Link to={'/'} className='text-white'>
+          BetweenStudents.com
+        </Link>
+      </div>
+    </MDBFooter>
   );
 };
 
