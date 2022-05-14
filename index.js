f0f8b513d211c20728183c097df52899539a42a1
Paul Miller <paul_john.miller0008@stud.hkr.se>
2022-05-15 01:12:17 +0200

    fixed server config

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, 'public')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
   });
 }
 
