7d2176af287e1862e0317a7293853c832ec18f28
Paul Miller <paul_john.miller0008@stud.hkr.se>
2022-05-15 21:03:40 +0200

    clean up before deploying

--- client/src/components/layout/LandingPage.js
+++ client/src/components/layout/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import useProducts from '../../hooks/useProducts';
 import { Link } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css';
@@ -16,12 +16,8 @@ const LandingHome = () => {
   const decorations = require('../../assets/decorations.jpg');
   const stationery = require('../../assets/stationery.jpg');
   const lighting = require('../../assets/lighting.jpg');
-  const [ProductState, productDispatch] = useProducts();
-  const { products } = ProductState;
-
-  useEffect(() => {
-    console.log('products', products);
-  }, [products]);
+  const productState = useProducts()[0];
+  const { products } = productState;
 
   const loading = useLoading(1500);
 
@@ -55,81 +51,81 @@ const LandingHome = () => {
   };
 
   return (
-    <div class='container-lg-sm-md flex-direction-column'>
-      <div class='card text-white'>
-        <img src={landingPic} class='card-img' alt='...' />
-        <div class='card-img-overlay div__overlayCard'>
-          <div class='row gx-5'>
-            <h5 class='card-title mt-5 h5__landing'>
+    <div className='container-lg-sm-md flex-direction-column'>
+      <div className='card text-white'>
+        <img src={landingPic} className='card-img' alt='...' />
+        <div className='card-img-overlay div__overlayCard'>
+          <div className='row gx-5'>
+            <h5 className='card-title mt-5 h5__landing'>
               A world of <br></br> Second-Hand <br></br>Magic!
             </h5>
-            <p class='card-text p__landing'>
+            <p className='card-text p__landing'>
               Modern, simple and minimal impact on our neighborhood and environment.
               <br></br>
             </p>
-            <div class='link__register'>
-              <Link to='/categories' class='btn btn-light my-3 hover__link'>
+            <div className='link__register'>
+              <Link to='/categories' className='btn btn-light my-3 hover__link'>
                 Shop now
               </Link>
             </div>
           </div>
         </div>
       </div>
-      <div class='row gx-5'>
-        <div class='col-md-6 mt-5 mb-3'>
-          <h5 class='font__aboveCat'>Check out our most popular products for home</h5>
+      <div className='row gx-5'>
+        <div className='col-md-6 mt-5 mb-3'>
+          <h5 className='font__aboveCat'>Check out our most popular products for home</h5>
         </div>
-        <div class='float__right col-md-6 mt-5 mb-3'>
+        <div className='float__right col-md-6 mt-5 mb-3'>
           <Link to={'/categories'}>
             <h5>View all</h5>
           </Link>
         </div>
       </div>
-      <div class='row gx-5'>
-        <div class='col-md-4 mb-5 div__catHover'>
+      <div className='row gx-5'>
+        <div className='col-md-4 mb-5 div__catHover'>
           <Link to={'/categories/furniture'}>
-            <img class='img-fluid img__catHover' alt='img' src={stationery}></img>
+            <img className='img-fluid img__catHover' alt='img' src={stationery}></img>
           </Link>
           <p>Furniture</p>
         </div>
-        <div class='col-md-4 mb-5 div__catHover'>
+        <div className='col-md-4 mb-5 div__catHover'>
           <Link to={`/categories/lamps & lights`}>
-            <img class='img-fluid img__catHover' alt='img' src={lighting}></img>
+            <img className='img-fluid img__catHover' alt='img' src={lighting}></img>
           </Link>
           <p>Lamps</p>
         </div>
-        <div class='col-md-4 mb-5 div__catHover'>
+        <div className='col-md-4 mb-5 div__catHover'>
           <Link to={'/categories/home decorations'}>
-            <img class='img-fluid img__catHover' alt='img' src={decorations}></img>
+            <img className='img-fluid img__catHover' alt='img' src={decorations}></img>
           </Link>
           <p>Home decorations</p>
         </div>
       </div>
-      <div class='row gx-5 mx-1 mb-5 pt-3 div__aboutApp'>
-        <div class='col-md-6'>
-          <img class='img-fluid' alt='img' src={sofa}></img>
+      <div className='row gx-5 mx-1 mb-5 pt-3 div__aboutApp'>
+        <div className='col-md-6'>
+          <img className='img-fluid' alt='img' src={sofa}></img>
         </div>
-        <div class='col-md-6'>
-          <div class='row-md-4 text__underHeaderThree'>
+        <div className='col-md-6'>
+          <div className='row-md-4 text__underHeaderThree'>
             <p>
               Between Students makes it easy and convenient for you to reuse and recycle
               your stuff. Making our neighborhood a better and cleaner place is our
               mission. Reduce impact on our environment is our passion.
             </p>
           </div>
-          <div class='row-md-2 link__mission'>
-            <Link to='/mission' class='btn btn-light my-3 hover__mission'>
+          <div className='row-md-2 link__mission'>
+            <Link to='/mission' className='btn btn-light my-3 hover__mission'>
               Read more
             </Link>
           </div>
         </div>
       </div>
-      <div class='container'>
-        <div class='row gx-5'>
-          <div class='col-md-6 mt-5 mb-3'>
-            <h5 class='font__aboveCat'>Check out our favourite products</h5>
+      <div className='container'>
+        <div className='row gx-5'>
+          <div className='col-md-6 mt-5 mb-3'>
+            <h5 className='font__aboveCat'>Check out our favourite products</h5>
           </div>
-          <div class='float__right col-md-6 mt-5 mb-3'>
+          <div className='float__right col-md-6 mt-5 mb-3'>
             <Link to={'/search'}>
               <h5>View all</h5>
             </Link>
@@ -139,18 +135,18 @@ const LandingHome = () => {
         {products !== null ? (
           <Slider {...settings}>
             {products.map((product) => (
-              <div class='out' key={product._id}>
-                <div class='card_carousel'>
+              <div className='out' key={product._id}>
+                <div className='card_carousel'>
                   <img
-                    class='img__carousel'
+                    className='img__carousel'
                     src={url + product.productImage}
                     alt={product._id}
                     height={200}
                     width={210}
                   ></img>
-                  <div class='card-body cardBody__carousel'>
-                    <p class='card-title cardTitle__carousel'>{product.title}</p>
-                    <small class='card-text text-muted cardTitle__carousel'>
+                  <div className='card-body cardBody__carousel'>
+                    <p className='card-title cardTitle__carousel'>{product.title}</p>
+                    <small className='card-text text-muted cardTitle__carousel'>
                       Price {product.cost} kr
                     </small>
                     <br></br>
@@ -164,19 +160,19 @@ const LandingHome = () => {
         )}
       </div>
 
-      <div class='card bg-white my-5 text-dark'>
-        <img src={dog} class='card-img' alt='...'></img>
-        <div class='card-img-overlay divOverlay__bottom'>
-          <div class='row gx-5'>
-            <h5 class='card-title mt-5 h5__landing'>
+      <div className='card bg-white my-5 text-dark'>
+        <img src={dog} className='card-img' alt='...'></img>
+        <div className='card-img-overlay divOverlay__bottom'>
+          <div className='row gx-5'>
+            <h5 className='card-title mt-5 h5__landing'>
               Love Our<br></br> neighborhood!<br></br>
             </h5>
-            <p class='card-text p__landing'>
+            <p className='card-text p__landing'>
               Come and join the movement.
               <br></br>
             </p>
-            <div class='link__register'>
-              <Link to={'/register'} class='btn btn-light my-3 hover__link'>
+            <div className='link__register'>
+              <Link to={'/register'} className='btn btn-light my-3 hover__link'>
                 Join Us!
               </Link>
             </div>
--- client/src/components/layout/Mission.js
+++ client/src/components/layout/Mission.js
@@ -9,14 +9,14 @@ const Mission = () => {
   const surprised = require('../../assets/woman-blue.jpg');
 
   return (
-    <div class='container-sm flex-direction-column'>
-      <div class='container-sm'>
-        <img class='img-fluid' alt='img' src={titlePage}></img>
+    <div className='container-sm flex-direction-column'>
+      <div className='container-sm'>
+        <img className='img-fluid' alt='img' src={titlePage}></img>
       </div>
-      <div class='container-sm py-3 text__header'>
-        <p class='textEmbed'>OUR MISSION</p>
+      <div className='container-sm py-3 text__header'>
+        <p className='textEmbed'>OUR MISSION</p>
         <h2>From throwing away to better use.</h2>
-        <div class='container-sm text__underHeader flex-direction-row'>
+        <div className='container-sm text__underHeader flex-direction-row'>
           <p>
             We know how difficult it is to move to a new city to study.<br></br> We also
             know how stressful it is to get rid of things you used <br></br> but no longer
@@ -25,17 +25,17 @@ const Mission = () => {
             University.
           </p>
           <img
-            class='img-fluid img__leaves'
+            className='img-fluid img__leaves'
             alt='img'
             src='https://djc49bfkedftq.cloudfront.net/fit-in/110x110/filters:format(png)/webApp/stickers/sticker_leaves_green.png'
           ></img>
         </div>
       </div>
-      <div class='row gx-5 div__wrapperCards'>
-        <div class='col-md-3 wrapper__img'>
-          <img class='img-fluid img__insideCard' alt='img' src={chairs}></img>
+      <div className='row gx-5 div__wrapperCards'>
+        <div className='col-md-3 wrapper__img'>
+          <img className='img-fluid img__insideCard' alt='img' src={chairs}></img>
         </div>
-        <div class='col-md-9 text__underHeaderTwo'>
+        <div className='col-md-9 text__underHeaderTwo'>
           <p>
             Does this photo look familiar to you who live close by Kristianstad
             University? <br></br>When people ditch their stuff out in the open, or beside
@@ -46,10 +46,10 @@ const Mission = () => {
           </p>
         </div>
       </div>
-      <div class='container-sm pt-5 text__header'>
-        <p class='textEmbed'>WHAT WE DO</p>
+      <div className='container-sm pt-5 text__header'>
+        <p className='textEmbed'>WHAT WE DO</p>
         <h2>A platform developed only for students.</h2>
-        <div class='text__underHeader'>
+        <div className='text__underHeader'>
           <p>
             'Between Students' is a trading platform only for students <br></br> who study
             in Kristianstad University.<br></br> On 'Between Students', you can put your
@@ -59,31 +59,31 @@ const Mission = () => {
           </p>
         </div>
       </div>
-      <div class='row gx-5 py-3'>
-        <div class='col-md-6 text__underHeaderThree'>
+      <div className='row gx-5 py-3'>
+        <div className='col-md-6 text__underHeaderThree'>
           <p>
             By using 'Between Students', you are not only helping to make our collective
             living areas look cleaner, but also are helping to reduce the impact of
             consumption on climate.
           </p>
         </div>
-        <div class='col-md-6'>
-          <img class='img-fluid' alt='img' src={amused}></img>
+        <div className='col-md-6'>
+          <img className='img-fluid' alt='img' src={amused}></img>
         </div>
       </div>
-      <div class='row gx-5 py-3 pb-5 flex-direction-column'>
-        <div class='col-md-6'>
-          <img class='img-fluid' alt='img' src={surprised}></img>
+      <div className='row gx-5 py-3 pb-5 flex-direction-column'>
+        <div className='col-md-6'>
+          <img className='img-fluid' alt='img' src={surprised}></img>
         </div>
-        <div class='col-md-6'>
-          <div class='row-md-4 text__underHeaderThree'>
+        <div className='col-md-6'>
+          <div className='row-md-4 text__underHeaderThree'>
             <p>
               We made it super easy and simple for you to start trading on our platform.
               <br />
             </p>
           </div>
-          <div class='row-md-2 link__register'>
-            <Link to='/register' class='btn btn-dark my-3 hover__link'>
+          <div className='row-md-2 link__register'>
+            <Link to='/register' className='btn btn-dark my-3 hover__link'>
               Read more
             </Link>
           </div>
