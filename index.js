5a13f6fe0c265fd756e9b024d61dd5ddde77b7aa
Paul Miller <paul_john.miller0008@stud.hkr.se>
2022-05-16 11:16:43 +0200

    Changed index.js to proper version

--- index.js
+++ index.js
@@ -21,15 +21,23 @@ app.use(cors());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-// Handle json body data
-app.use(express.json());
-
-
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/products', productRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+  app.use('/', express.static(path.join(__dirname, 'public')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   signale.success(`Server listening on port ${PORT}`);
