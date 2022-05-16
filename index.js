31b10c795dc76d5c1322986d0cd46d79862831af
Paul Miller <paul_john.miller0008@stud.hkr.se>
2022-05-16 21:35:28 +0200

    removed unnecessary endpoint

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use('/', express.static(path.join(__dirname, 'public')));
-
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/products', productRoutes);
